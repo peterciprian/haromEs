216e42fc8c0422c9deae679047a62ed895564f09
peterciprian <peterciprian@gmail.com>
2018-10-23 13:23:53 +0200

    footer & scrolltop

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -10,6 +10,28 @@ export class FooterComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    window.addEventListener('scroll', this.scroll, true);
+}
+
+  // tslint:disable-next-line:use-life-cycle-interface
+  ngOnDestroy() {
+    window.removeEventListener('scroll', this.scroll, true);
+  }
+
+  scroll = (): void => {
+    this.scrollFunction();
+  }
+
+  scrollFunction() {
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+      document.getElementById('myBtn').style.display = 'block';
+    } else {
+      document.getElementById('myBtn').style.display = 'none';
+    }
+  }
+  topFunction() {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
   }
 
 }
