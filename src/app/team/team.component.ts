d75a764d00d896a65e6c7ddedfc4cbf57485c7a4
peterciprian <peterciprian@gmail.com>
2018-11-02 14:14:52 +0100

    team ngFor-ral

--- src/app/team/team.component.ts
+++ src/app/team/team.component.ts
@@ -7,6 +7,15 @@ import { Renderer } from '@angular/core';
   styles: []
 })
 export class TeamComponent implements OnInit {
+  protected team = [
+    { name: 'Benkő Krisztina', desc: 'BK Lorem', top: 9, left: 9 },
+    { name: 'Oláh Péter Ciprián', desc: 'OPC Lorem', top: 18, left: 71 },
+    { name: 'Vadász Dániel Pál', desc: 'VDP Lorem', top: 21, left: 40 },
+    { name: 'Lakner Lóránd László', desc: 'LLL Lorem', top: 42, left: 16 },
+    { name: 'Lehotai Bence Gábor', desc: 'LBG Lorem', top: 50, left: 53 },
+    { name: 'Gyimesi Levente Zsombor', desc: 'GyLZs Lorem', top: 64, left: 75 },
+    { name: 'Treszkai Anett', desc: 'TA Lorem', top: 71, left: 34 }
+  ];
 
   constructor(private render: Renderer) { }
 
@@ -15,6 +24,6 @@ export class TeamComponent implements OnInit {
 
   select(event: Event) {
     event.preventDefault();
-    console.log(event.target);
+    console.log(this.render);
   }
 }
